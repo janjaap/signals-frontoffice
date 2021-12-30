deb2d974844eb40b4528f84a19d9331b7a815810
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-12-30 08:50:21 +0100

    Adds @svgr

--- next.config.js
+++ next.config.js
@@ -9,4 +9,12 @@ module.exports = {
       },
     ]
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
